095e42ad143024fb2deaf7f895262963183b2745
Grace Eunice <2382025@unai.edu>
2024-09-24 14:44:19 +0700

    Handson4

--- ContohReturnValue.java
+++ ContohReturnValue.java
@@ -1,11 +1,17 @@
 public class ContohReturnValue {
+
     public static int tambah(int a, int b){
         int hasil = a + b;
         return hasil;
     }
-
+    public static boolean apakahGenap(int angka) {
+        return angka % 2 == 0;
+    }
+    public static String sapaan(String nama){
+        return "Halo, " + nama + "!";
+        }
     public static void main(String[] args) {
         int hasilPenjumlahan = tambah(3,5);
-        System.out.println("3+5" + hasilPenjumlahan);
+        System.out.println("3+5 = " + hasilPenjumlahan);
     }
 }
